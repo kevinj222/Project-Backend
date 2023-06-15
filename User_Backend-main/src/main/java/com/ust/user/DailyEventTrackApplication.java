709416dd8ac0b9a37ff52d107acfa6e07f2c62b5
Kevin kodiyan <kevinjolly222@gmail.com>
2023-06-15 23:27:37 +0530

    1st commit

---
+++ User_Backend-main/src/main/java/com/ust/user/DailyEventTrackApplication.java
@@ -0,0 +1,21 @@
+package com.ust.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyEventTrackApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DailyEventTrackApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
